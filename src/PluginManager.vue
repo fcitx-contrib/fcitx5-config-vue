c0c0d854883536e3a7fa0f9fcefc74811b0c098d
Qijia Liu <liumeo@pku.edu.cn>
2024-12-16 21:01:30 -0500

    plugin manager

---
+++ src/PluginManager.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import type { UploadFileInfo } from 'naive-ui'
+import { NA, NFlex, NList, NListItem, NUpload, NUploadDragger, useMessage } from 'naive-ui'
+import { computed, ref } from 'vue'
+
+const message = useMessage()
+
+function getInstalledPlugins() {
+  return window.fcitx.getInstalledPlugins().sort()
+}
+
+const allPlugins = ['anthy', 'chewing', 'chinese-addons', 'hallelujah', 'hangul', 'lua', 'rime', 'sayura', 'thai', 'unikey']
+const installedPlugins = ref<string[]>(getInstalledPlugins())
+const availablePlugins = computed(() => allPlugins.filter(plugin => !installedPlugins.value.includes(plugin)))
+
+const fileList = ref<UploadFileInfo[]>([])
+
+async function onUpload(files: UploadFileInfo[]) {
+  // Must clear the fileList synchronously first as uploading multiple files will trigger multiple times.
+  fileList.value = []
+  for (const file of files) {
+    let name: string
+    const buffer = await file.file!.arrayBuffer()
+    try {
+      name = window.fcitx.installPlugin(buffer)
+    }
+    catch (e: any) {
+      message.error(e.message)
+      continue
+    }
+    window.fcitx.updateStatusArea()
+    installedPlugins.value = getInstalledPlugins()
+    message.success(`Installed ${name}`)
+  }
+}
+</script>
+
+<template>
+  <NFlex size="large">
+    <NUpload v-model:file-list="fileList" style="width: auto" multiple accept=".zip" @update:file-list="onUpload">
+      <NUploadDragger style="height: 200px">
+        Download and drag zip to this area
+      </NUploadDragger>
+    </NUpload>
+    <NFlex>
+      <NList style="min-width: 100px">
+        <template #header>
+          Installed
+        </template>
+        <NListItem v-for="plugin in installedPlugins" :key="plugin">
+          {{ plugin }}
+        </NListItem>
+      </NList>
+      <NList style="min-width: 100px">
+        <template #header>
+          Available
+        </template>
+        <NListItem v-for="plugin in availablePlugins" :key="plugin">
+          <NA :href="`https://github.com/fcitx-contrib/fcitx5-plugins/releases/download/js/${plugin}.zip`">
+            {{ plugin }}
+          </NA>
+        </NListItem>
+      </NList>
+    </NFlex>
+  </NFlex>
+</template>
