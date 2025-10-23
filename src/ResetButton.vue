1748fc08254214496674e2f1c69a1203e3e47853
Qijia Liu <liumeo@pku.edu.cn>
2025-10-23 19:39:38 -0400

    outline gear button; add reset button

---
+++ src/ResetButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { NDialogProvider } from 'naive-ui'
+import ResetButtonInner from './ResetButtonInner.vue'
+</script>
+
+<template>
+  <NDialogProvider>
+    <ResetButtonInner />
+  </NDialogProvider>
+</template>
---
+++ src/ResetButtonInner.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { NButton, useDialog } from 'naive-ui'
+import { t } from './i18n'
+
+const dialog = useDialog()
+
+function click() {
+  dialog.warning({
+    title: t('Warning'),
+    content: t('Your user data and installed plugins will be removed.'),
+    positiveText: t('OK'),
+    negativeText: t('Cancel'),
+    onPositiveClick: async () => {
+      await window.fcitx.reset()
+      window.location.reload()
+    },
+  })
+}
+</script>
+
+<template>
+  <NButton type="error" secondary @click="click">
+    {{ t('Reset') }}
+  </NButton>
+</template>
