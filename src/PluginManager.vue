d6d6d2593bde54a294d723764484bc799a49c50d
Qijia Liu <liumeo@pku.edu.cn>
2025-11-28 12:17:53 -0500

    mobile adaption: plugin manager

--- src/PluginManager.vue
+++ src/PluginManager.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import type { UploadFileInfo } from 'naive-ui'
-import { NA, NFlex, NList, NListItem, NText, NUpload, NUploadDragger, useMessage } from 'naive-ui'
-import { computed, ref } from 'vue'
+import type { MenuOption, UploadFileInfo } from 'naive-ui'
+import { NA, NFlex, NMenu, NScrollbar, NText, NUpload, NUploadDragger, useMessage } from 'naive-ui'
+import { computed, h, ref } from 'vue'
 import { t } from './i18n'
 import MustInstall from './MustInstall.vue'
+import { isMobile } from './util'
 
 defineProps<{
   disabled: boolean
@@ -15,9 +16,53 @@ function getInstalledPlugins() {
   return window.fcitx.getInstalledPlugins().sort()
 }
 
-const allPlugins = ['anthy', 'chewing', 'chinese-addons', 'hallelujah', 'hangul', 'jyutping', 'lua', 'm17n', 'mozc', 'rime', 'sayura', 'skk', 'thai', 'unikey']
+const plugins: [string, string][] = [
+  ['anthy', 'Japanese'],
+  ['chewing', 'Chinese'],
+  ['chinese-addons', 'Chinese'],
+  ['hallelujah', 'English'],
+  ['hangul', 'Korean'],
+  ['jyutping', 'Chinese'],
+  ['lua', 'Other'],
+  ['m17n', 'Generic'],
+  ['mozc', 'Japanese'],
+  ['rime', 'Generic'],
+  ['sayura', 'Sinhala'],
+  ['skk', 'Japanese'],
+  ['thai', 'Thai'],
+  ['unikey', 'Vietnamese'],
+]
+
+function tLanguage(language: string) {
+  return t(`language.${language}`)
+}
+
+function toOptions(plugins: [string, string][]) {
+  const pluginMap: Record<string, string[]> = {}
+  for (const [plugin, language] of plugins) {
+    (pluginMap[language] = pluginMap[language] || []).push(plugin)
+  }
+  return Object.entries(pluginMap).sort(([la], [lb]) => {
+    for (const key of ['Other', 'Generic']) {
+      if (la === key) {
+        return 1
+      }
+      if (lb === key) {
+        return -1
+      }
+    }
+    return tLanguage(la).localeCompare(tLanguage(lb))
+  }).map(([language, plugins]) => ({
+    type: 'group',
+    key: language,
+    label: tLanguage(language),
+    children: plugins.map(plugin => ({ key: plugin, label: plugin })),
+  }))
+}
+
 const installedPlugins = ref<string[]>(getInstalledPlugins())
-const availablePlugins = computed(() => allPlugins.filter(plugin => !installedPlugins.value.includes(plugin)))
+const installedOptions = computed(() => toOptions(plugins.filter(([plugin, _]) => installedPlugins.value.includes(plugin))))
+const availableOptions = computed(() => toOptions(plugins.filter(([plugin, _]) => !installedPlugins.value.includes(plugin))))
 
 const fileList = ref<UploadFileInfo[]>([])
 
@@ -39,44 +84,44 @@ async function onUpload(files: UploadFileInfo[]) {
     message.success(t('Installed {plugin}', { plugin: name }))
   }
 }
+
+function installedItem(option: MenuOption) {
+  if (option.key === 'jyutping' && !installedPlugins.value.includes('chinese-addons')) {
+    return h(MustInstall, { plugin: 'chinese-addons' }, { default: () => h(NText, {
+      delete: true,
+    }, { default: option.label }) })
+  }
+  return option.label as string
+}
+
+function availableItem(option: MenuOption) {
+  return h(MustInstall, { plugin: option.key === 'jyutping' ? 'chinese-addons' : undefined }, { default: () => h(NA, {
+    href: `https://github.com/fcitx-contrib/fcitx5-plugins/releases/download/js/${option.key}.zip`,
+  }, { default: option.label }) })
+}
 </script>
 
 <template>
-  <NFlex size="large">
-    <NUpload v-model:file-list="fileList" :disabled="disabled" multiple accept=".zip" @update:file-list="onUpload">
-      <NUploadDragger style="height: 200px">
-        <p>{{ t('Download plugin zip by clicking links below') }}</p>
-        <p>{{ t('then click (or drag them to) this area') }}</p>
+  <NFlex size="large" :vertical="isMobile" :style="{ height: isMobile ? '100%' : 'calc(100vh - 100px)' }">
+    <NUpload v-model:file-list="fileList" :style="isMobile ? {} : { width: 'auto', flex: '1' }" :disabled="disabled" multiple accept=".zip" @update:file-list="onUpload">
+      <NUploadDragger :style="isMobile ? {} : { height: '300px' }">
+        <p>{{ t('Download plugin zip by clicking buttons under "Available"') }}</p>
+        <p>{{ isMobile ? t('then click this area to load zip') : t('then click (or drag them to) this area to load zip') }}</p>
       </NUploadDragger>
     </NUpload>
-    <NFlex>
-      <NList style="min-width: 100px">
-        <template #header>
-          {{ t('Installed') }}
-        </template>
-        <NListItem v-for="plugin in installedPlugins" :key="plugin">
-          <MustInstall v-if="plugin === 'jyutping' && !installedPlugins.includes('chinese-addons')" plugin="chinese-addons">
-            <NText delete>
-              {{ plugin }}
-            </NText>
-          </MustInstall>
-          <template v-else>
-            {{ plugin }}
-          </template>
-        </NListItem>
-      </NList>
-      <NList style="min-width: 100px">
-        <template #header>
-          {{ t('Available') }}
-        </template>
-        <NListItem v-for="plugin in availablePlugins" :key="plugin">
-          <MustInstall :plugin="plugin === 'jyutping' ? 'chinese-addons' : undefined">
-            <NA :href="`https://github.com/fcitx-contrib/fcitx5-plugins/releases/download/js/${plugin}.zip`">
-              {{ plugin }}
-            </NA>
-          </MustInstall>
-        </NListItem>
-      </NList>
+    <NFlex :style="isMobile ? { 'min-height': '0' } : { height: '100%', flex: '1' }">
+      <NFlex vertical style="flex: 1; height: 100%">
+        {{ t('Installed') }}
+        <NScrollbar>
+          <NMenu :root-indent="0" :indent="24" :options="installedOptions" :render-label="installedItem" />
+        </NScrollbar>
+      </NFlex>
+      <NFlex vertical style="flex: 1; height: 100%">
+        {{ t('Available') }}
+        <NScrollbar>
+          <NMenu :root-indent="0" :indent="24" :options="availableOptions" :render-label="availableItem" />
+        </NScrollbar>
+      </NFlex>
     </NFlex>
   </NFlex>
 </template>
