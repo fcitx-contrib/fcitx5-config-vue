17f386458ff371108e9868e05eb701086e935fde
Panda527 <whiredplanck@outlook.com>
2026-08-23 04:38:48 +0800

    regex validation for string config option (#10)

--- src/option/StringOption.vue
+++ src/option/StringOption.vue
@@ -1,15 +1,45 @@
 <script setup lang="ts">
-import { NInput } from 'naive-ui'
+import { NFormItem, NInput } from 'naive-ui'
+import { computed } from 'vue'
+import { t } from '../i18n'
 
-defineProps<{
+const props = defineProps<{
+  config: { [key: string]: any }
   value: string
   onUpdate: (value: string) => void
 }>()
+
+function isRegex(config: { [key: string]: any }) {
+  return config.IsRegex === 'True'
+    || ((config.ListConstrain ?? {}).IsRegex === 'True')
+}
+
+function isRegexValid(value: string) {
+  return window.fcitx.Module.ccall('is_regex_valid', 'boolean', ['string'], [value])
+}
+
+const isValid = computed(() => {
+  const text = props.value
+  return !isRegex(props.config) || isRegexValid(text)
+})
+
+const inputValidationStatus = computed(() => {
+  return isValid.value ? undefined : 'error'
+})
+
+const inputFeedback = computed(() => isValid.value ? undefined : t('Invalid regular expression'))
 </script>
 
 <template>
-  <NInput
-    :value="value"
-    @update:value="onUpdate"
-  />
+  <NFormItem
+    :validation-status="inputValidationStatus"
+    :feedback="inputFeedback"
+    :show-label="false"
+    :show-feedback="!isValid"
+  >
+    <NInput
+      :value="value"
+      @update:value="onUpdate"
+    />
+  </NFormItem>
 </template>
